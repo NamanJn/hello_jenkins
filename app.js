a4dd3b7ce64efde8441270f297a5b5441e93aa9c
Your Name <nj1411@ic.ac.uk>
2015-12-17 15:01:53 +0000

    changed response

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 var express = require('express');
 var app = express();
 
-console.log("lskdjfldskj");
 
 
 app.get('/', function (req, res) {
-	  res.send('hello wworldsldkjf');
+	  res.send('hello musk');
 })
 
 
