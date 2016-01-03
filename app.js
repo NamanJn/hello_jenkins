a3ce76009734eb4b7f52b429fc89cfa39450c4a4
Naman Jain <nj1411@ic.ac.uk>
2016-01-03 21:38:58 +0000

    Added a new URL.

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.get('/item5', function (req, res) {
 	  res.send('Hey This is item 5!!'+ req.originalUrl)
 })
 
+app.get('/item6', function (req, res) {
+	  res.send("This is in neowne")
+})
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
